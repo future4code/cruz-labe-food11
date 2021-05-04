e1c84ac103fcd0e58c426b9299a6575320c1db4e
Adryane Fernandes <edryfernandes@gmail.com>
2021-05-04 14:13:56 -0300

    lista de restaurantes funcionando

--- src/constants/headersConfig.js
+++
@@ -1,5 +0,0 @@
-export const headersConfig = {
-  headersConfig: {
-    auth: localStorage.getItem('token')
-  }
-}
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,21 @@
+import { Box } from '@chakra-ui/layout'
 import React from 'react'
 import useRequestData from '../../hooks/useRequestData'
 
 function HomePage(){
-  const restaurants = useRequestData({}, 'restaurants')
-  
-  return <div>HomePage</div>
+  const restaurants = useRequestData([], 'restaurants')
+
+  const listRestaurants = restaurants.restaurants && restaurants.restaurants.map((restaurant) => {
+    return (
+      <Box border='1px solid black'>
+        <img src={restaurant.logoUrl} alt={'logo do restaurante'}/>
+        <h3>{restaurant.name}</h3>
+      </Box>
+    )
+  })
+  return(
+    <div>{listRestaurants}</div>
+  )
 }
 
 export default HomePage
