8f24525fa360c954781ebce57ecf3aa3405643e2
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-05 15:37:54 -0300

    resolvido problema do required password, faltando estilizar . Signup configurado para se as senhas nao forem iguais, não passar de page.

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -6,66 +6,63 @@ import useProtectedPage from '../../hooks/useProtectedPage'
 import logout from '../../functions/logout'
 import styled from 'styled-components'
 function HomePage() {
-  
+
   useProtectedPage()
 
   const restaurants = useRequestData([], 'restaurants')
   const history = useHistory()
-  const [filteredRestaurants,setFilteredRestaurants] = useState([])
+  const [filteredRestaurants, setFilteredRestaurants] = useState(restaurants)
 
   const goToDetails = (id) => {
     history.push(`/restaurante/${id}`)
   }
 
 
-  
-
-const filterFunc = (event) => {
-  const search = event.target.value.toLowerCase()
-  const newRestaurants = restaurants.restaurants.filter((restaurant) =>restaurant.name.toLowerCase().indexOf(search) >=0)
-  setFilteredRestaurants(newRestaurants)
-  
-
+  const filterFunc = (event) => {
+    const search = event.target.value.toLowerCase()
+    const newRestaurants = restaurants.restaurants.filter((restaurant) => restaurant.name.toLowerCase().indexOf(search) >= 0)
+    setFilteredRestaurants(newRestaurants)
   }
-      const listRestaurants =  restaurants.restaurants && restaurants.restaurants.map((restaurant) => {
-        return (
-          <Box border='1px solid black'
-            key={restaurant.id}
-            onClick={() => goToDetails(restaurant.id)}
-          >
-            <img src={restaurant.logoUrl} alt={'logo do restaurante'} />
-            <h3>{restaurant.name}</h3>
-          </Box>
-        )
-      })
+  const listRestaurants = restaurants.restaurants && restaurants.restaurants.map((restaurant) => {
+    return (
+      <Box border='1px solid black'
+        key={restaurant.id}
+        onClick={() => goToDetails(restaurant.id)}
+      >
+        <img src={restaurant.logoUrl} alt={'logo do restaurante'} />
+        <h3>{restaurant.name}</h3>
+      </Box>
+    )
+  })
 
-    const filtered = filteredRestaurants.map((restaurant) => {
+  const filtered = filteredRestaurants.map((restaurant) => {
 
-        return (
-          <Box border='1px solid black'
-            key={restaurant.id}
-            onClick={() => goToDetails(restaurant.id)}
-          >
-            <img src={restaurant.logoUrl} alt={'logo do restaurante'} />
-            <h3>{restaurant.name}</h3>
-    
-          </Box>
-        )
-      })
+    return (
+      <Box border='1px solid black'
+        key={restaurant.id}
+        onClick={() => goToDetails(restaurant.id)}
+      >
+        <img src={restaurant.logoUrl} alt={'logo do restaurante'} />
+        <h3>{restaurant.name}</h3>
 
-   return (
-     <div>
-       <button onClick={() => logout(history)}> logout </button>
-       <Input onChange={filterFunc} type='text' placeholder="Busca por restaurantes"/>
-       {filteredRestaurants.length>0 ? filtered : <div> Nada encontrado</div> }
-     </div>
-    
+      </Box>
+    )
+  })
+
+  return (
+    <div>
+      <button onClick={() => logout(history)}> logout </button>
+      <Input onChange={filterFunc} type='text' placeholder="Busca por restaurantes" />
+      {/* {filteredRestaurants.length>0 ? filtered : <div> Nada encontrado</div> } */}
+      {listRestaurants}
+    </div>
+
   )
 
 }
 
 export default HomePage
 
-const Input   = styled.input`
+const Input = styled.input`
 border:1px solid black;
 `
--- src/pages/SignupPage/SignUpPage.js
+++ src/pages/SignupPage/SignUpPage.js
@@ -10,46 +10,43 @@ import { IconButton } from '@chakra-ui/button'
 const SignUpPage = () => {
 
     const [form, onChange, clear] = useForm({ name: '', email: '', password: '', cpf: '' })
+    const [formPassword,onChangePassword,clearPassword] = useForm({ confirmPassword: ''})
     const history = useHistory()
-    const [confirmPassword, setConfirmPassword] = useState('')
     const [passwordType, setPasswordType] = useState('password')
     const [hidePassword, setHidePassword] = useState(true)
     const [confirmCheckPasswordType, setConfirmCheckPasswordType] = useState('password')
     const [hideCheckPassword, setHideCheckPassword] = useState(true)
-    const [validity,setValidity] = useState('')
+    const [display, setDisplay] = useState('none')
+    const [isCompletedData,setIsCompletedData] = useState(false)
     useEffect(() => {
         console.log("useEffect")
-        console.log('form.password: ', form.password)
-        console.log('checkpassword: ', confirmPassword)
     }, [form.password])
 
 
     const checkPassword = (event) => {
-        setConfirmPassword(event.target.value)
-
-        // console.log('form : ', form.password)
-        // console.log("confirmPassword: ", confirmPassword)
-        // console.log('validity: ', validity)
+        onChangePassword(event)
+            setIsCompletedData(true)
         if (event.target.value.length > 5 && form.password.length > 5 && event.target.value !== form.password) {
-            event.target.setCustomValidity("As senhas não são iguais.")
-            // setValidity('As senhas não são iguais.')
-            // setValidity(event.target.value)
-            // console.log("la dentrooooooooooo!")
-            // console.log('validity: ',validity)
+            setIsCompletedData(false)
+            setDisplay('block')
         } else {
+            setIsCompletedData(true)
+            setDisplay('none')
         }
     }
 
     const check = (event) => {
+        onChange(event)
         console.log("onInput check: ", event.target.value)
-        // console.log('form : ',form)
-        if (event.target.value !== form.password) {
-            event.target.setCustomValidity("As senhas não são iguais.")
-    }else{
-        event.target.setCustomValidity("")
-        console.log('entrei no else')
-    }}
-    
+        if (event.target.value.length > 5 && form.password.length > 5 && formPassword.confirmPassword != event.target.value) {
+            setIsCompletedData(false)
+            setDisplay('block')
+        } else {
+            setDisplay('none')
+            setIsCompletedData(true)
+        }
+    }
+
     const changeTypePassword = () => {
         setHidePassword(!hidePassword)
         if (hidePassword) {
@@ -74,7 +71,7 @@ const SignUpPage = () => {
 
             </BoxLogo>
             <H4>Cadastrar</H4>
-            <Form onSubmit={(evt) => loginOrSignUp('signup', form, history, evt)}>
+            <Form onSubmit={(evt) => loginOrSignUp('signup', form, history, evt,isCompletedData)}>
                 <Input name="name" value={form.name} onChange={onChange} placeholder="Nome e sobrenome" type='text' required pattern={"^.{6,}"} />
                 <Input name="email" value={form.email} onChange={onChange} placeholder="email@email.com" type='email' required pattern={"[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,}$"} />
                 <Input name="cpf" value={form.cpf} onChange={onChange} placeholder="000.000.000-00" type='text' required pattern={'([0-9]{3}[\.][0-9]{3}[\.][0-9]{3}[-][0-9]{2})'} />
@@ -88,10 +85,13 @@ const SignUpPage = () => {
                             />
                         }
                     />
-                    <Input name="password" value={form.password} onChange={onChange} placeholder="Mínimo 6 caracteres" type={passwordType} required pattern={"^.{6,}"} setCustomValidity={validity} />
-                    {/* onInput={check2} */}
+                       
+                    <Input name="password" value={form.password} 
+                     placeholder="Mínimo 6 caracteres" type={passwordType} required pattern={"^.{6,}"}  
+                    onChange={check} 
+                    />
                 </InputGroup>
-
+                <div style={{ display: `${display}` }}>As senhas não são iguais.</div>
                 <InputGroup>
                     <InputRightElement
                         children={
@@ -102,14 +102,11 @@ const SignUpPage = () => {
                             />
                         }
                     />
-                    <Input name="checkPassword" value={confirmPassword} onChange={checkPassword} placeholder="Confirme a senha anterior" type={confirmCheckPasswordType} required pattern={"^.{6,}"} 
-                    onInput={check}
+                    <Input name="confirmPassword" value={formPassword.confirmPassword} onChange={checkPassword} placeholder="Confirme a senha anterior" type={confirmCheckPasswordType} required pattern={"^.{6,}"}
                     />
                 </InputGroup>
-
                 <Button type='submit'>Criar</Button>
             </Form>
-
         </Container>
     )
 }
