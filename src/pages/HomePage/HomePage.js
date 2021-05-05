1d2251ae4f759c81e290837a5e9681bb80ebb314
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-05 17:19:27 -0300

    provisoriamente feito o state inicial da lista de restaurantes

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -5,17 +5,35 @@ import { Box } from '@chakra-ui/layout'
 import useProtectedPage from '../../hooks/useProtectedPage'
 import logout from '../../functions/logout'
 import styled from 'styled-components'
+import { BASE_URL } from '../../constants/url'
+import axios from 'axios'
 function HomePage() {
 
   useProtectedPage()
 
+
+  const [filteredRestaurants, setFilteredRestaurants] = useState(useRequestData([], 'restaurants'))
+
   const restaurants = useRequestData([], 'restaurants')
   const history = useHistory()
-  const [filteredRestaurants, setFilteredRestaurants] = useState(restaurants)
 
   const goToDetails = (id) => {
     history.push(`/restaurante/${id}`)
   }
+  useEffect(( ) => {
+    console.log("filtered: ",filteredRestaurants )
+    setRestaurants()
+    },[])
+
+const setRestaurants = async () => {
+const response = await axios.get(`${BASE_URL}restaurants`,{
+  headers: {
+    auth: localStorage.getItem('token')
+  }
+})
+console.log('setRestaurants: ',response.data)
+setFilteredRestaurants(response.data.restaurants)
+}
 
 
   const filterFunc = (event) => {
@@ -23,6 +41,8 @@ function HomePage() {
     const newRestaurants = restaurants.restaurants.filter((restaurant) => restaurant.name.toLowerCase().indexOf(search) >= 0)
     setFilteredRestaurants(newRestaurants)
   }
+
+
   const listRestaurants = restaurants.restaurants && restaurants.restaurants.map((restaurant) => {
     return (
       <Box border='1px solid black'
@@ -35,11 +55,11 @@ function HomePage() {
     )
   })
 
-  const filtered = filteredRestaurants.map((restaurant) => {
+  const filtered =  restaurants && restaurants.restaurants && filteredRestaurants.map((restaurant) => {
 
     return (
       <Box border='1px solid black'
-        key={restaurant.id}
+        key={restaurant.key}
         onClick={() => goToDetails(restaurant.id)}
       >
         <img src={restaurant.logoUrl} alt={'logo do restaurante'} />
@@ -53,8 +73,12 @@ function HomePage() {
     <div>
       <button onClick={() => logout(history)}> logout </button>
       <Input onChange={filterFunc} type='text' placeholder="Busca por restaurantes" />
-      {/* {filteredRestaurants.length>0 ? filtered : <div> Nada encontrado</div> } */}
-      {listRestaurants}
+      {/* {restaurants && restaurants.restaurants && listRestaurants.length>0 && setFilteredRestaurants(restaurants)} */}
+      {filteredRestaurants.length>0 ? filtered : <div> Nada encontrado</div> }
+      {/* {listRestaurants} */}
+      {console.log("restaurants: ",restaurants)}
+      {console.log("filteredRestaurants: ",filteredRestaurants)}
+      {/* {console.log("filteredRestaurants: ",filteredRestaurants)} */}
     </div>
 
   )
@@ -65,4 +89,5 @@ export default HomePage
 
 const Input = styled.input`
 border:1px solid black;
+display:block;
 `
