259863610ab3954d4ce53957bbeedae6d84b20de
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-05 12:22:25 -0300

    core da pesquisa feita. Falta editar pra aparecer inicialmente , nao depois de clicar em algo...

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,34 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import useRequestData from '../../hooks/useRequestData'
 import { Box } from '@chakra-ui/layout'
 import useProtectedPage from '../../hooks/useProtectedPage'
 import logout from '../../functions/logout'
+import styled from 'styled-components'
 function HomePage() {
+  
   useProtectedPage()
 
   const restaurants = useRequestData([], 'restaurants')
   const history = useHistory()
+  const [filteredRestaurants,setFilteredRestaurants] = useState([])
 
   const goToDetails = (id) => {
     history.push(`/restaurante/${id}`)
   }
 
-  const listRestaurants = restaurants.restaurants && restaurants.restaurants.map((restaurant) => {
-    return (
-      <Box border='1px solid black'
-        key={restaurant.id}
-        onClick={() => goToDetails(restaurant.id)}
-      >
-        <img src={restaurant.logoUrl} alt={'logo do restaurante'} />
-        <h3>{restaurant.name}</h3>
-      </Box>
-    )
-  })
+
+  
+
+const filterFunc = (event) => {
+  const search = event.target.value.toLowerCase()
+  const newRestaurants = restaurants.restaurants.filter((restaurant) =>restaurant.name.toLowerCase().indexOf(search) >=0)
+  setFilteredRestaurants(newRestaurants)
+  
+
+  }
+      const listRestaurants =  restaurants.restaurants && restaurants.restaurants.map((restaurant) => {
+        return (
+          <Box border='1px solid black'
+            key={restaurant.id}
+            onClick={() => goToDetails(restaurant.id)}
+          >
+            <img src={restaurant.logoUrl} alt={'logo do restaurante'} />
+            <h3>{restaurant.name}</h3>
+          </Box>
+        )
+      })
+
+    const filtered = filteredRestaurants.map((restaurant) => {
+
+        return (
+          <Box border='1px solid black'
+            key={restaurant.id}
+            onClick={() => goToDetails(restaurant.id)}
+          >
+            <img src={restaurant.logoUrl} alt={'logo do restaurante'} />
+            <h3>{restaurant.name}</h3>
+    
+          </Box>
+        )
+      })
+
    return (
      <div>
        <button onClick={() => logout(history)}> logout </button>
-       <div>{listRestaurants}</div>
+       <Input onChange={filterFunc} type='text' placeholder="Busca por restaurantes"/>
+       {filteredRestaurants.length>0 ? filtered : <div> Nada encontrado</div> }
      </div>
     
   )
@@ -36,3 +65,7 @@ function HomePage() {
 }
 
 export default HomePage
+
+const Input   = styled.input`
+border:1px solid black;
+`
