c01dec6687da3daf7f0725c3e6dcda38a757506f
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-06 18:34:05 -0300

    Div que aparece quando as senhas nao sao iguais foi editada

--- src/pages/SignupPage/SignUpPage.js
+++ src/pages/SignupPage/SignUpPage.js
@@ -199,8 +199,14 @@ export default SignUpPage
 
 const DivIsDifferentPassword = styled.div`
 height:1.125rem;
-width:12.063rem;
+width:13.063rem;
 color:#e02020;
-margin-left:2rem;
+margin: 0.313rem 7.438rem 0 1rem;
 font-size:0.75rem;
+font-family: Roboto;
+font-weight: normal;
+font-stretch: normal;
+font-style: normal;
+line-height: normal;
+letter-spacing: -0.29px
 `
