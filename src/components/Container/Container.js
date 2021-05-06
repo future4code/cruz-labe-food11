b6a29a0905ef045dd9d0927f2dee9badf9bc4846
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-06 18:59:25 -0300

    Header praticamente pronto, falta adicionar nas paginas e editar mais 1 coisa para algumas paginas

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -3,9 +3,7 @@ import styled from 'styled-components'
 export const Container = styled.div`
 display:flex;
 flex-direction:column;
-/* margin:10vh auto 0; */
 align-items:center;
 min-height:100vh;
 width:22.5rem;
-/* background-color:orange; */
 `
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import backIcon from '../../assets/back.svg'
 import React from 'react'
 
-const Header = () => {
+const Header = (props) => {
 
     return (
         <Container>
@@ -11,6 +11,7 @@ const Header = () => {
             <GoBackIcon>
 
             </GoBackIcon>
+            <TitleHeader>{props.text}</TitleHeader>
         </Container>
     )
 }
@@ -27,12 +28,20 @@ padding: 0 0 0.625rem;
 backdrop-filter: blur(10px);
 box-shadow: 0 0.5px 0 0 rgba(0, 0, 0, 0.25);
 background-color: #ffffff;
+display:flex;
 
 `
 const GoBackIcon = styled.div`
 background-image:url(${backIcon});
 width:1.5rem;
 height:1.44rem;
-margin:1.85rem 20.06rem 0.625rem 1rem;
+margin:1.85rem 0 0.625rem 1rem;
 
+`
+
+const TitleHeader = styled.div`
+  width: 10.938rem;
+  height: 2.75rem;
+  margin: 1.25rem 5.75rem 0 3.373rem;
+  padding: 0.813rem 2.844rem 0.75rem;
 `
