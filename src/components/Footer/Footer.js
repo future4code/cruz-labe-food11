0a9a489950bfbf7f003f34f8d7f0bc220bc1b911
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-06 17:34:03 -0300

    colocado o componente com letra maiuscula ( ButtonSubmit e InputForm) e editado os arquivos que os tinham

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Container } from '../Container/Container'
-import homePage from '../../assets/homepage.svg'
-import homePageActive from '../../assets/homepage-active.svg'
-import shoppingCart from '../../assets/shopping-cart.svg'
-import shoppingCartActive from '../../assets/shopping-cart-active.svg'
-import avatar from '../../assets/avatar.svg'
-import avatarActive from '../../assets/avatar-active.svg'
-
+import { Avatar, AvatarActive, BoxComponent, ContainerFooter, ContainerIcons, HomePage, HomePageActive, ShoppingCart, ShoppingCartActive } from './styled'
 
 const Footer = (props) => {
     return (
@@ -28,63 +21,4 @@ const Footer = (props) => {
     )
 }
 
-
 export default Footer
-
-const ContainerFooter = styled(Container)`
-/* position:fixed; */
-/* bottom:0; */
-width:22.5rem;
-`
-
-const ContainerIcons = styled.div`
-display:flex;
-/* width:100%; */
-/* border:1px solid black; */
-position:fixed;
-bottom:0;
-`
-
-const BoxComponent = styled.div`
-width: 7.5rem;
-height: 3.062rem;
-border:1px solid red;
-padding: 0.625rem 2.875rem 0.563rem 2.938rem;
-display:flex;
-`
-
-const HomePage = styled.div`
-background-image:url(${homePage});
-height:1.688rem;
-width:1.688rem;
-`
-
-const HomePageActive = styled.div`
-background-image:url(${homePageActive});
-height:1.688rem;
-width:1.688rem;
-`
-
-const ShoppingCart = styled.div`
-background-image:url(${shoppingCart});
-height:1.688rem;
-width:1.688rem;
-`
-
-const ShoppingCartActive = styled.div`
-background-image:url(${shoppingCartActive});
-height:1.688rem;
-width:1.688rem;
-`
-
-const Avatar = styled.div`
-background-image:url(${avatar});
-height:1.688rem;
-width:1.688rem;
-`
-
-const AvatarActive = styled.div`
-background-image:url(${avatarActive});
-height:1.688rem;
-width:1.688rem;
-`
