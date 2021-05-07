09f00f6fbb5a2c84af84ff4c03fbd58635a9c0ef
Anderson Oliveira <tandersonf@hotmail.com>
2021-05-07 15:04:44 -0300

    Tela inicial feita

---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { ContainerLogo, LoadingLogo } from './styled'
+
+const Loading = () => {
+
+    return (
+        <ContainerLogo>
+            <LoadingLogo />
+        </ContainerLogo>
+    )
+}
+
+export default Loading
+
